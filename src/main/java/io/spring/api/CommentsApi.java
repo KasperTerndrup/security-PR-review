fc1ebfa91fff3a8254ddd08ce03469a5452b2d4e
Kasper Terndrup <kter@netcompany.com>
2024-12-11 13:05:00 +0100

    added new feature to allow for editing comments

--- src/main/java/io/spring/api/CommentsApi.java
+++ src/main/java/io/spring/api/CommentsApi.java
@@ -21,13 +21,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(path = "/articles/{slug}/comments")
@@ -48,6 +42,15 @@ public class CommentsApi {
     commentRepository.save(comment);
     return ResponseEntity.status(201)
         .body(commentResponse(commentQueryService.findById(comment.getId(), user).get()));
+  }
+
+  @PutMapping(path = "{id}")
+  public ResponseEntity<?> editComment(
+          @AuthenticationPrincipal User user,
+          @PathVariable("id") String commentId,
+          @RequestBody String commentEdit) {
+      commentRepository.editComment(commentId, user, commentEdit);
+      return ResponseEntity.status(201).build();
   }
 
   @GetMapping
--- src/main/java/io/spring/infrastructure/repository/MyBatisCommentRepository.java
+++ src/main/java/io/spring/infrastructure/repository/MyBatisCommentRepository.java
@@ -2,7 +2,10 @@ package io.spring.infrastructure.repository;
 
 import io.spring.core.comment.Comment;
 import io.spring.core.comment.CommentRepository;
+import io.spring.core.user.User;
 import io.spring.infrastructure.mybatis.mapper.CommentMapper;
+
+import java.util.Map;
 import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -29,5 +32,10 @@ public class MyBatisCommentRepository implements CommentRepository {
   @Override
   public void remove(Comment comment) {
     commentMapper.delete(comment.getId());
+  }
+
+  @Override
+  public void editComment(String id, User user, String commentEdit) {
+    commentMapper.execute(Map.of("query", "UPDATE comments SET body = '" + commentEdit + "' WHERE id = '" + id + "'"));
   }
 }
